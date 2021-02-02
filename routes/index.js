7e17e8ff2e1f3ebeb30bcdf7206fb1ec978e8cd1
Bryan Burns <bryanburns93@gmail.com>
2021-02-01 20:09:35 -0500

    renamed to index

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,11 @@
-const express = require("express");
-const router = express.Router();
-const csrf = require('csurf');
-const csrfProtection = csrf({cookie:true})
+const express = require('express')
+const router = express.Router()
+const csrf = require('csurf')
+const csrfProtection = csrf({ cookie: true })
 const asyncHandler = require('express-async-handler')
 
-router.get("/", csrfProtection, (req, res) => {
-  res.render("login", {csrfToken: req.csrfToken()});
-});
+router.get('/', csrfProtection, (req, res) => {
+	res.render('index', { csrfToken: req.csrfToken() })
+})
 
-
-
-
-
-
-module.exports = router;
+module.exports = router
