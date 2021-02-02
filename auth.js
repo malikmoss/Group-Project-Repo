f513f925edf32fd3e7db94a6434a363517fdc057
Malik Moss <malikmoss33@gmail.com>
2021-02-02 12:22:33 -0600

    created hashed pw and added user to db

--- auth.js
+++
@@ -1,4 +0,0 @@
-const jwt = require("jsonwebtoken");
-const { jwtConfig } = require("./config");
-const { User } = require("./db/models");
-const bearerToken = require("express-bearer-token");
--- routes/users.js
+++ routes/users.js
@@ -1,18 +1,43 @@
 const express = require("express");
+const { check } = require("express-validator");
+const { handleValidationErrors, asyncHandler } = require("../utils");
 const router = express.Router();
-const asyncHandler = require('express-async-handler')
 const csrf = require('csurf');
-const csrfProtection = csrf({cookie:true})
+const csrfProtection = csrf({cookie:true});
+const bcrypt = require('bcryptjs');
+const { User } = require('../db/models');
+
+const validateUsername =
+  check("username")
+    .exists({ checkFalsy: true })
+    .withMessage("Please provide a username");
+
+const validateEmailAndPassword = [
+  check("email")
+    .exists({ checkFalsy: true })
+    .isEmail()
+    .withMessage("Please provide a valid email."),
+  check("password")
+    .exists({ checkFalsy: true })
+    .withMessage("Please provide a password."),
+];
+
 
 router.get("/", (req, res) => {
   res.render("login");
 });
 
-router.post('/', csrfProtection, asyncHandler(async (req, res) => {
-    const { username, email, password } = req.body
 
-
-
+router.post('/', 
+  csrfProtection, 
+  validateUsername, 
+  validateEmailAndPassword, 
+  handleValidationErrors, 
+  asyncHandler(async (req, res) => {
+    const { username, email, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await User.create({ username, email, hashedPassword })
+    res.redirect('/')
 }))
 
 module.exports = router;
