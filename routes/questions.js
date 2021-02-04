e1f0f9679171200ad24b93d6d697bcb7661346d8
Bryan Burns <bryanburns93@gmail.com>
2021-02-03 19:08:38 -0500

    start question styling

--- routes/questions.js
+++ routes/questions.js
@@ -3,30 +3,24 @@ const { asyncHandler } = require('../utils')
 const { restoreUser, requireAuth } = require('../auth')
 const { User, Que } = require('../db/models')
 
-router.get('/', restoreUser, requireAuth, asyncHandler(async(req, res) => {
-	// console.log(req.session.auth.userId) Get userid to display account info
-	const quesQuery = await Que.findAll({
-		include: [{ model: User, attributes: ["username"] }],
-		order: [["createdAt", "DESC"]],
-		attributes: ["body"],
+router.get(
+	'/',
+	restoreUser,
+	requireAuth,
+	asyncHandler(async (req, res) => {
+		// console.log(req.session.auth.userId) Get userid to display account info
+		const quesQuery = await Que.findAll({
+			include: [{ model: User, attributes: ['username'] }],
+			order: [['createdAt', 'DESC']],
+			attributes: ['body'],
+		})
+
+		const ques = quesQuery.map(que => ({ username: que.User.username, body: que.body }))
+		const data = {
+			ques,
+		}
+		res.render('questions', data)
 	})
-
-	// console.log(quesQuery)
-
-	const ques = quesQuery.map(que => {
-		let obj = {}
-		console.log(que.User.username)
-		obj.username = que.User.username
-		obj.body = que.body
-	})
-
-
-	const data = {
-		ques
-	}
-	res.render('questions', data)
-}))
-
-
+)
 
 module.exports = router
