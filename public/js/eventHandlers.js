91ba80d7814d2d49df1b444f19b85bd466f2ad7d
Bryan Burns <bryanburns93@gmail.com>
2021-02-05 06:22:24 -0500

    Added back button functionality

--- public/js/eventHandlers.js
+++ public/js/eventHandlers.js
@@ -1,22 +1,8 @@
 window.addEventListener('DOMContentLoaded', () => {
-	document.querySelector('.add-que').addEventListener('click', () => {
-		const question = document.querySelector('.new-question')
-		fetch('/questions', {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json',
-			},
-			body: JSON.stringify(question.value),
+	const back = document.querySelector('.button__back')
+	if (back) {
+		back.addEventListener('click', () => {
+			history.go(-1)
 		})
-	})
-	document.querySelectorAll('.edit-que').forEach(b => {
-		b.addEventListener('click', e => {
-			const id = e.path[1].id.slice(4)
-		})
-	})
-	document.querySelectorAll('.delete-que').forEach(b => {
-		b.addEventListener('click', e => {
-			const id = e.path[1].id.slice(4)
-		})
-	})
+	}
 })
