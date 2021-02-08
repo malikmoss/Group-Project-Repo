5eeebda4e43a7642e47c0764c8c61e0f04919d8d
Bryan Burns <bryanburns93@gmail.com>
2021-02-08 00:13:54 -0500

    create new answer

--- public/js/components/que.js
+++ public/js/components/que.js
@@ -1,18 +1,21 @@
 window.addEventListener('DOMContentLoaded', () => {
-    document.querySelector('.add-answer').addEventListener('click', e => {
-        // const adj = 12 - e.path.length
-        // const path = 4 - adj
-        // const queId = e.path[path].id.slice(4)
-        fetch('/answers', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-
-            body: {
-                questionId: queId,
-                body: document.querySelector('#addAnswer').value
-            }
-        })
-    })
+	document.querySelector('.add-answer').addEventListener('click', e => {
+		const data = {
+			queId: document.querySelector('.container__que').id.slice(4),
+			text: document.querySelector('#addAnswer').value,
+		}
+		fetch('/answers', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(data),
+		})
+			.then(res => {
+				if (res.status === 200) return res.json()
+			})
+			.then(data => {
+				console.log(data)
+			})
+	})
 })
--- routes/answers.js
+++ routes/answers.js
@@ -3,41 +3,42 @@ const { asyncHandler } = require('../utils')
 const { restoreUser, requireAuth } = require('../auth')
 const { User, Que, Answer, Vote, Comment } = require('../db/models')
 
-
 router.get(
 	'/',
 	restoreUser,
 	// requireAuth,
 	asyncHandler(async (req, res) => {
-
 		const answerQuery = await Answer.findAll({
 			include: [
-                { model: Que, attributes: ['id', 'body', 'authorId']},
-                //  where: [body= !null]},
-                { model: User, attributes: ['username', 'id'] },
+				{ model: Que, attributes: ['id', 'body', 'authorId'] },
+				//  where: [body= !null]},
+				{ model: User, attributes: ['username', 'id'] },
 				{
-                    model: Comment,
+					model: Comment,
 
-                    attributes: ['authorId', 'body'],
-                    include: [
-                        {model: User, attributes: ['username']},
-                    ]
+					attributes: ['authorId', 'body'],
+					include: [{ model: User, attributes: ['username'] }],
 				},
 			],
 			order: [['createdAt', 'DESC']],
 			attributes: ['body', 'id', 'authorId'],
-        })
+		})
 
-        const answers = answerQuery.map(answer => ({question: answer.Que, id: answer.id, author: answer.User.username, body: answer.body, comments: answer.Comments
-            // .map(comment => ({
-            //     // id: comment.id,
-            //     body: comment.body,
-            //     authorId: comment.authorId,
-            //     username: comment.User.username
+		const answers = answerQuery.map(answer => ({
+			question: answer.Que,
+			id: answer.id,
+			author: answer.User.username,
+			body: answer.body,
+			comments: answer.Comments,
+			// .map(comment => ({
+			//     // id: comment.id,
+			//     body: comment.body,
+			//     authorId: comment.authorId,
+			//     username: comment.User.username
 
-            // }))
-        }))
-        res.send(answers)
+			// }))
+		}))
+		res.send(answers)
 	})
 )
 
@@ -46,7 +47,7 @@ router.get(
 	restoreUser,
 	// requireAuth,
 	asyncHandler(async (req, res) => {
-		const id = (req.params.id)
+		const id = req.params.id
 		const answer = await Answer.findByPk(id, {
 			include: [
 				{
@@ -62,30 +63,24 @@ router.get(
 			attributes: ['body'],
 			include: [{ model: User, attributes: ['username'] }],
 		})
-        res.send([answer, comments])
+		res.send([answer, comments])
 		// res.render('que', { title: que.body, que, answers })
 	})
 )
 
 router.post(
-    '/',
+	'/',
 	restoreUser,
-	// requireAuth,
+	requireAuth,
 	asyncHandler(async (req, res) => {
-        console.log('test')
-        console.log(res.locals.user.id)
-        console.log(req.body.queId)
-        console.log(req.body.id,)
 		const answer = await Answer.create({
-            authorId: res.locals.user.id,
-            questionId: req.body.queId,
-			body: req.body.body
-        })
-        // res.send(answer)
+			authorId: res.locals.user.id,
+			questionId: req.body.queId,
+			body: req.body.text,
+		})
 		res.json(answer)
 	})
 )
-
 
 // router.patch(
 // 	'/:id',
