abef0e4ea7ce9547b2a79cc76b09bbbe51534058
Bryan Burns <bryanburns93@gmail.com>
2021-05-17 22:41:40 -0400

    add onDelete to migrations

--- db/migrations/20210203224749-create-answer.js
+++ db/migrations/20210203224749-create-answer.js
@@ -1,38 +1,40 @@
-'use strict';
+'use strict'
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('Answers', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      questionId: {
-        allowNull: false,
-        type: Sequelize.INTEGER,
-        references: { model: 'Ques'}
-      },
-      authorId: {
-        allowNull: false,
-        type: Sequelize.INTEGER,
-        references: { model: 'Users'}
-      },
-      body: {
-        allowNull: false,
-        type: Sequelize.TEXT,
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('Answers');
-  }
-};
+	up: (queryInterface, Sequelize) => {
+		return queryInterface.createTable('Answers', {
+			id: {
+				allowNull: false,
+				autoIncrement: true,
+				primaryKey: true,
+				type: Sequelize.INTEGER,
+			},
+			questionId: {
+				allowNull: false,
+				type: Sequelize.INTEGER,
+				references: { model: 'Ques' },
+				onDelete: 'CASCADE',
+			},
+			authorId: {
+				allowNull: false,
+				type: Sequelize.INTEGER,
+				references: { model: 'Users' },
+				onDelete: 'CASCADE',
+			},
+			body: {
+				allowNull: false,
+				type: Sequelize.TEXT,
+			},
+			createdAt: {
+				allowNull: false,
+				type: Sequelize.DATE,
+			},
+			updatedAt: {
+				allowNull: false,
+				type: Sequelize.DATE,
+			},
+		})
+	},
+	down: (queryInterface, Sequelize) => {
+		return queryInterface.dropTable('Answers')
+	},
+}
--- db/models/que.js
+++ db/models/que.js
@@ -1,14 +1,18 @@
-'use strict';
+'use strict'
 module.exports = (sequelize, DataTypes) => {
-  const Que = sequelize.define('Que', {
-    body: DataTypes.STRING,
-    authorId: DataTypes.INTEGER
-  }, {});
-  Que.associate = function(models) {
-    // associations can be defined here
-    Que.belongsTo(models.User, { foreignKey: 'authorId' });
-    Que.hasMany(models.Vote, { foreignKey: 'questionId' });
-    Que.hasMany(models.Answer, { foreignKey: 'questionId' });
-  };
-  return Que;
-};
+	const Que = sequelize.define(
+		'Que',
+		{
+			body: DataTypes.STRING,
+			authorId: DataTypes.INTEGER,
+		},
+		{}
+	)
+	Que.associate = function (models) {
+		// associations can be defined here
+		Que.belongsTo(models.User, { foreignKey: 'authorId' })
+		Que.hasMany(models.Vote, { foreignKey: 'questionId' })
+		Que.hasMany(models.Answer, { foreignKey: 'questionId' })
+	}
+	return Que
+}
